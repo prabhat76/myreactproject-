676359f575c8b6d9d8bf577c2cca3eb867c24ca7
Prabhat Kumar <prabhatkumar@Prabhats-MacBook-Pro.local>
2024-07-28 13:34:40 +0530

    Addition of Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@
 import './App.css';
 
 
-let name = "Prabhat";
+
 function App() {
   return (
    <>
-   <div className='Heading'><h1>welcome to {name}'s portfolio</h1></div>
-   <div className='List'>
-    <nav>
-      <li>Home</li>
-      <li>Contact </li>
-      <li>Work</li>
-    </nav>
-   </div>
-   <div className='container'>
-      <p></p>
-   </div>
-   
+ 
+ <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">MY Test Application</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
    
    
    </>
